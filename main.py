5fe085f97ec830cdd27afb860913d5781813f268
TuZhi <tuzhitz@163.com>
2019-04-22 22:58:00 +0800

    docM

--- main.py
+++ main.py
@@ -34,15 +34,17 @@ def gen_nm(num):
 
 
 def get_image_info(image):
-    print('type(image)', type(image))   # type() 函数如果只有第一个参数则返回对象的类型   在这里函数显示图片类型为 numpy类型的数组
-    print('image.shape', image.shape)
+    print('================= get image info =================')
+    print('== type(image): ', type(image))   # type() 函数如果只有第一个参数则返回对象的类型   在这里函数显示图片类型为 numpy类型的数组
+    print('== image.shape: ', image.shape)
     # 图像矩阵的shape属性表示图像的大小，shape会返回tuple元组，
     # 第一个元素表示矩阵行数，第二个元组表示矩阵列数，第三个元素是3，表示像素值由光的三原色组成
     # print('image.size', image.size)   # 返回图像的大小，size的具体值为shape三个元素的乘积
-    print('image.dtype', image.dtype)  # 数组元素的类型通过dtype属性获得
-    pixel_data = np.array(image)
-    print(pixel_data.shape)
+    print('== image.dtype: ', image.dtype)  # 数组元素的类型通过dtype属性获得
+    # pixel_data = np.array(image)
+    # print(pixel_data.shape)
     # print(pixel_data)  # 打印图片矩阵     N维数组对象即矩阵对象
+    print('--------------------------------------------------')
 
 
 def gen_video(in_path, size, version):
@@ -214,6 +216,7 @@ def main():
     imgset = 'alley_1/'
     imgset_pth = os.path.join(MPI_PTH, subset_pth, imgset)
 
+    # # ==================================================
     # # calculate average image
     # print(imgset_pth)
     # img = avg_img(imgset_pth, 11, 2)
@@ -221,14 +224,19 @@ def main():
     # cv2.namedWindow('test_image', cv2.WINDOW_AUTOSIZE)
     # cv2.imshow('test_image', img)
     # cv2.waitKey(0)
+    # # --------------------------------------------------
 
+    # # ==================================================
     # # visualize optical flow by video
-    # video_pth = os.path.join(OW_PTH, 'Overwatch_v02.mp4')
-    # print(video_pth)
-    # gen_opt_flow_video(video_pth)
+    video_pth = os.path.join(OW_PTH, 'Overwatch_v02.mp4')
+    print(video_pth)
+    gen_opt_flow_video(video_pth)
+    # # --------------------------------------------------
 
+    # # ==================================================
     # calculate optical flow by images
     gen_opt_flow_img(imgset_pth)
+    # # --------------------------------------------------
 
 
 if __name__ == '__main__':

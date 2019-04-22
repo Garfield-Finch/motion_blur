8bb3892032c3f2e33f3bf7433f391da553772acf
TuZhi <tuzhitz@163.com>
2019-04-22 20:27:27 +0800

    add generate video

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import cv2
 from PIL import Image
 import numpy as np
+import time
 
 
 DATASET = '../dataset/MPI-Sintel-complete/'
+VIDEO_PTH = '../video/'
 
 
 def gen_nm(num):
@@ -22,6 +24,31 @@ def get_image_info(image):
     pixel_data = np.array(image)
     print(pixel_data.shape)
     # print(pixel_data)  # 打印图片矩阵     N维数组对象即矩阵对象
+
+
+def gen_video(in_path, size, version):
+
+    filelist = os.listdir(in_path)  # 获取该目录下的所有文件名
+
+    '''
+    fps:
+    帧率：1秒钟有n张图片写进去[控制一张图片停留5秒钟，那就是帧率为1，重复播放这张图片5次] 
+    如果文件夹下有50张 534*300的图片，这里设置1秒钟播放5张，那么这个视频的时长就是10秒
+    '''
+    fps = 12
+    # size = (591,705) #图片的分辨率片
+    out_pth = VIDEO_PTH + "{0:02d}.avi".format(version)  # 导出路径
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 不同视频编码对应不同视频格式（例：'I','4','2','0' 对应avi格式）
+
+    video = cv2.VideoWriter(out_pth, fourcc, fps, size)
+
+    for item in filelist:
+        if item.endswith('.png'):  # 判断图片后缀是否是.png
+            item = in_path + item
+            img = cv2.imread(item)  # 使用opencv读取图像，直接返回numpy.ndarray 对象，通道顺序为BGR ，注意是BGR，通道值默认范围0-255。
+            video.write(img)  # 把图片写进视频
+
+    video.release()  # 释放
 
 
 def avg_img(imgset_pth, img_n, mix_n):
@@ -44,12 +71,18 @@ def main():
     imgset = 'alley_1/'
 
     imgset_pth = os.path.join(DATASET, subset_pth, imgset)
-    print(imgset_pth)
-    img = avg_img(imgset_pth, 11, 2)
 
-    cv2.namedWindow('test_image', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('test_image', img)
-    cv2.waitKey(0)
+    # # calculate average image
+    # print(imgset_pth)
+    # img = avg_img(imgset_pth, 11, 2)
+    # # visualize average image
+    # cv2.namedWindow('test_image', cv2.WINDOW_AUTOSIZE)
+    # cv2.imshow('test_image', img)
+    # cv2.waitKey(0)
+
+    # generate video from images
+    img = cv2.imread(os.path.join(imgset_pth, gen_nm(1)))
+    gen_video(imgset_pth, img.shape[:2], 1)
 
 
 if __name__ == '__main__':

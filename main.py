487e22b2cbd9bf51869a4dd0c4188c59e423708c
TuZhi <tuzhitz@163.com>
2019-04-22 22:41:04 +0800

    add optical flow for image set video generate codec error

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ OW_PTH = '../dataset/Overwatch/'
 VIDEO_PTH = '../video/'
 
 
+class IterImage:
+    def __init__(self, pth):
+        self.filelist = os.listdir(pth)
+        self.position = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.position >= len(self.filelist):
+            raise StopIteration
+
+        self.position += 1
+        ret_img = self.filelist[self.position]
+        ret_img = np.array(ret_img)
+        return ret_img
+
+
 def gen_nm(num):
     ans = 'frame_{0:04d}.png'.format(num)
     return ans
@@ -75,9 +93,8 @@ def avg_img(imgset_pth, img_n, mix_n):
     return img
 
 
-def gen_opt_flow(video_pth):
+def gen_opt_flow_video(video_pth):
     cap = cv2.VideoCapture(video_pth)
-    # cap = cv2.VideoCapture('slow.flv')
 
     # params for ShiTomasi corner detection
     feature_params = dict(maxCorners=100,
@@ -95,7 +112,6 @@ def gen_opt_flow(video_pth):
 
     # Take first frame and find corners in it
     ret, old_frame = cap.read()
-    print(type(old_frame))
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
     p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
 
@@ -134,6 +150,63 @@ def gen_opt_flow(video_pth):
     cap.release()
 
 
+def gen_opt_flow_img(img_pth):
+    cap = IterImage(img_pth)
+
+    # params for ShiTomasi corner detection
+    feature_params = dict(maxCorners=100,
+                          qualityLevel=0.3,
+                          minDistance=7,
+                          blockSize=7)
+
+    # Parameters for lucas kanade optical flow
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=2,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+    # Create some random colors
+    color = np.random.randint(0, 255, (100, 3))
+
+    # Take first frame and find corners in it
+    old_frame = next(cap)
+
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
+
+    while 1:
+        frame = next(cap)
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+        # Select good points
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
+
+        # draw the tracks
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+            frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+        img = cv2.add(frame, mask)
+
+        cv2.imshow('frame', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+
+    cv2.destroyAllWindows()
+
+
 def main():
 
     subset_pth = 'training/albedo/'
@@ -149,10 +222,13 @@ def main():
     # cv2.imshow('test_image', img)
     # cv2.waitKey(0)
 
-    # calculate optical flow
-    video_pth = os.path.join(OW_PTH, 'Overwatch_v01.mp4')
-    print(video_pth)
-    gen_opt_flow(video_pth)
+    # # visualize optical flow by video
+    # video_pth = os.path.join(OW_PTH, 'Overwatch_v02.mp4')
+    # print(video_pth)
+    # gen_opt_flow_video(video_pth)
+
+    # calculate optical flow by images
+    gen_opt_flow_img(imgset_pth)
 
 
 if __name__ == '__main__':

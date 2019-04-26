492ea81e42680dcd8b08dce3a56ac027a5eba52b
TuZhi <tuzhitz@163.com>
2019-04-26 21:13:38 +0800

    update

--- utils/gen_kernel.m
+++ utils/gen_kernel.m
@@ -50,11 +50,18 @@ figure(2), imshow(img_o), title('img\_gen');
 % img_o = inpaintCoherent(img_o, img_mask);
 % figure(3), imshow(img_o), title('img\_inpaint');
 
+
+%% same output image
+img_nm = [img_pth, gen_nm(img_epoch, 3)];
+disp(['img2save name: ', img_nm]);
+imwrite(img_o, img_nm);
+
 %% Utils
 function imgnm = gen_nm(num, key)
     % key == 0, input image name
     % key == 1, output interpolation image name
     % key == 2, .flo file name
+    % key == 3, _ker.png
     if key == 0
         s = num2str(num);
         len = length(s);
@@ -76,6 +83,13 @@ function imgnm = gen_nm(num, key)
             s = ['0', s];
         end
         imgnm = ['frame_', s, '.flo'];
+    elseif key == 3
+        s = num2str(num);
+        len = length(s);
+        for i = 1:4-len
+            s = ['0', s];
+        end
+        imgnm = ['frame_', s, '_ker.png'];
     else
         disp('KEY ERROR !!!')
     end
